a2fcc37a545cf72da418f848cc8418c0b879ee64
yeojin Kim <jjiny2022@kakao.com>
2022-07-18 18:30:22 +0900

    220718

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <HeaderSection />
+  <SliderSection />
+  <ImageSection skill="section" />
+  <imgtextSection skill="section" color="grey" />
+  <TextSection />
+  <CardSection skill="section" />
+  <BannerSection />
+  <FooterSection />
+</template>
+
+<script>
+import HeaderSection from './components/HeaderSection.vue';
+import SliderSection from './components/SliderSection.vue';
+import ImageSection from './components/ImageSection.vue';
+import ImgtextSection from './components/ImgtextSection.vue';
+import CardSection from './components/CardSection.vue';
+import BannerSection from './components/BannerSection.vue';
+import FooterSection from './components/FooterSection.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderSection,
+    SliderSection,
+    ImageSection,
+    ImgtextSection,
+    CardSection,
+    BannerSection,
+    FooterSection,
+  },
+};
+</script>
+
+<style>
+@import './App.css';
+</style>
